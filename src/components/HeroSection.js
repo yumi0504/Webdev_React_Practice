2be8b226de30d8af12b0bae94ed0aef6c16a92c0
yumi0504 <yumichoi0504@gmail.com>
2025-05-13 21:31:51 +0930

    Include main code in main repo

---
+++ src/components/HeroSection.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
+      <h1>SOMEONE HAD TO CARE</h1>
+      <p>To Infinity and Beyond</p>
+      <div className="hero-btns">
+          <Button 
+            className='btns'
+            buttonStyle='btn--outline'
+            bottonSize='btn--large'
+            to='/about'
+          >
+            ABOUT 
+          </Button>
+          <Button 
+            className='btns'
+            buttonStyle='btn--primary'
+            bottonSize='btn--large'
+            to='/profile'
+          >
+            PROFILE<i className='far fa-play-cycle' />
+          </Button>
+      </div>
+    </div>
+  )
+}
+
+
+export default HeroSection
---
+++ src/components/pages/Story.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Story() {
+  return <h1 className='story'>STORY</h1>;
+}
